d9d2b790ad94402044f84a7685f32bea1b26801c
Ivan <ivanjimenez1101@gmail.com+>
2025-01-23 18:52:58 +0100

    Ejercicios string terminados

--- 03.1_ejercicios_string.py
+++ 03.1_ejercicios_string.py
@@ -11,4 +11,100 @@ string_principal = string1 + " " + string2 + " " + string3 + " " + string4
 print(string_principal)
 print("--------------------------------------------------------------------------")
 
+print("Ejercicios 4 y 5")
+print("--------------------------------------------------------------------------")
+
+compañia = "Coding for all"
+print(compañia)
+print(len(compañia))
+
+print("--------------------------------------------------------------------------")
+print("utiliza el metodo upper para convertir el string en mayusculas")
+print("--------------------------------------------------------------------------")
+print(compañia.upper())
+print("--------------------------------------------------------------------------")
+print("utiliza el metodo ulower para convertir el string en minusculas")
+print("--------------------------------------------------------------------------")
+print(compañia.lower())
+print("--------------------------------------------------------------------------")
+print("utiliza el metodo capitalize, title y swapcase para convertir el string en capitalizar")
+print("--------------------------------------------------------------------------")
+print(compañia.capitalize())
+print(compañia.title())
+print(compañia.swapcase())
+print("--------------------------------------------------------------------------")
+print("Recorta la primera palabra de coding for all")
+print("--------------------------------------------------------------------------")
+print(compañia[0:6])
+print("--------------------------------------------------------------------------")
+print("comprueba si la cadena contiene Coding")
+print("--------------------------------------------------------------------------")
+print(compañia.find("Coding"))
+print(compañia.index("Coding"))
+print("Coding" in compañia)
+print("--------------------------------------------------------------------------")
+print("reemplaza el texto 'Coding' por 'Python'")
+compañia = "Python for all"
+print("--------------------------------------------------------------------------")
+print("Ejercicio 12")
+print(compañia.replace("for all", "for everyone"))
+print("--------------------------------------------------------------------------")
+print("Ejercicio 13")
+compañia = "Coding for all"
+print(compañia.split(" "))
+print("--------------------------------------------------------------------------")
+print("Ejercicio 14")
+compañia = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+print(compañia.split(","))
+print("--------------------------------------------------------------------------")
+print("Ejercicio 15")
+compañia = "Coding for all"
+print(compañia[0])
+print("--------------------------------------------------------------------------")
+print("Ejercicio 16")
+print(compañia[-1])
+print("--------------------------------------------------------------------------")
+print("Ejercicio 17")
+print(compañia[10])
+print("--------------------------------------------------------------------------")
+print("Ejercicio 18")
+compañia = "Python For Everyone"
+acronym = ''.join(word[0] for word in compañia.split())
+print(acronym)
+print("--------------------------------------------------------------------------")
+print("Ejercicio 19")
+compañia = "Coding For All"
+acronym = "".join(word[0] for word in compañia.split())
+print(acronym)
+print("--------------------------------------------------------------------------")
+print("Ejercicio 20,21 y 22")
+
+print(compañia.index("C"))
+print(compañia.index("F"))
+print(compañia.index("l"))
+print(compañia.rfind("l"))
+print("--------------------------------------------------------------------------")
+print("Ejercicio 23, 24 y 25")
+frase = 'You cannot end a sentence with because because because is a conjunction'
+print(frase.index('because'))
+print(frase.rindex('because'))
+print(frase[31:55])
+print("--------------------------------------------------------------------------")
+print("Ejercicio 28 y 29")
+compañia = "Coding For All"
+print(compañia.startswith("Coding"))
+print(compañia.endswith("coding"))
+print("--------------------------------------------------------------------------")
+print("Ejercicio 30")
+compañia = "  Coding For All  "
+
+print(compañia.strip(" "))
+print("--------------------------------------------------------------------------")
+print("Ejercicio 31")
+compañia = ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']
+print(compañia)
+print(" ".join(compañia))
+
+
+
 
--- readme.md
+++ readme.md
@@ -8,7 +8,7 @@ Este repositorio contiene ejemplos y ejercicios del curso de Python. A continuac
 - **01_variables.py**: Ejemplos de uso de variables, concatenación de cadenas, funciones del sistema y manejo de tipos de datos en Python.
 - **02_operadores.py**: Ejemplos de operadores aritméticos, lógicos y de comparación en Python.
 - **03_strings.py**: Ejemplos con string, divisiones y métodos
-- **03.1_ejercicios_string**: Ejercicios de string del repositorio "Treinta dias de Python" (todavía sin terminar)
+- **03.1_ejercicios_string**: Ejercicios de string del repositorio "Treinta dias de Python"
 
 ## Progreso
 
